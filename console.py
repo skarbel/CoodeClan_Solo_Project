e73f3da4910e33b9d36e953fabd7ee8627945683
Andreas Skarpelos <95192235+Skarbel@users.noreply.github.com>
2022-01-27 16:35:20 +0000

    added test capability and updated console file

---
+++ console.py
@@ -0,0 +1,6 @@
+import pdb
+from models.pet import Pets
+from models.customer import Customer
+
+import repositories.customer_repository as customer_repository
+import repositories.pet_repository as pet_repository

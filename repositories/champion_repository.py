5f3b3cfbf205421ca89c2dea9f9b2040cc7bbc29
Andreas Skarpelos <95192235+Skarbel@users.noreply.github.com>
2022-01-28 16:55:20 +0000

    coded the repositories file.

--- repositories/champion_repository.py
+++ repositories/champion_repository.py
@@ -2,9 +2,42 @@ from db.run_sql import run_sql
 from models.champion import Champion
 from models.skill import Skill
 
+
 def save(champion):
-    sql = "INSERT INTO champions( champion_name, champion_title, champion_class, release_date) VALUES ( %s, %s , %s, %s) RETURNING *"
+    sql = "INSERT INTO champions( champion_name, champion_title, champion_class, release_date) VALUES ( %s, %s , %s, %s ) RETURNING *"
     values = [champion.champion_name, champion.champion_title, champion.champion_class, champion.release_date]
     results = run_sql( sql, values )
     champion.id = results[0]['id']
     return champion
+
+def select(id):
+    champion = None
+    sql = "SELECT * FROM champions WHERE id = %s"
+    values = [id]
+    result = run_sql(sql, values)[0]
+    if result is not None:
+        champion = Champion(result['champion_name'], result['champion_title'], result['champion_class'], result['release_date'], result['id'])
+        return champion
+
+def select_all():
+    champions = []
+    sql = "SELECT * FROM champions"
+    results = run_sql(sql)
+    for row in results:
+        champion = Champion(row['champion_name'], row['champion_title'], row['champion_class'], row['release_date'], row['id'])
+        champions.append(champion)
+    return champions
+
+def delete(id):
+    sql = "DELETE  FROM champions WHERE id = %s"
+    values = [id]
+    run_sql(sql, values)
+    
+def delete_all():
+    sql = "DELETE  FROM champions"
+    run_sql(sql)
+
+def update(champion):
+    sql = "UPDATE champions SET (champion_name, champion_title, champion_class, release_date) = ( %s, %s , %s, %s ) WHERE id = %s"
+    values = [champion.first_name, champion.last_name, champion.id]
+    run_sql(sql, values)
--- repositories/skill_repository.py
+++ repositories/skill_repository.py
@@ -1,11 +1,45 @@
 from db.run_sql import run_sql
 from models.champion import Champion
 from models.skill import Skill
+import repositories.champion_repository as champion_repository # WHY?
 
 def save(skill):
-    sql = "INSERT INTO skills( champion_id, skill_name, skill_shortcut ) VALUES (%s, %s, %s) RETURNING *"
+    sql = "INSERT INTO skills ( champion_id, skill_name, skill_shortcut ) VALUES (%s, %s, %s) RETURNING *"
     values = [skill.champion.id, skill.skill_name, skill.skill_shortcut]
     results = run_sql( sql, values )
     skill.id = results[0]['id']
     return skill
 
+def select(id):
+    skill = None
+    sql = "SELECT * FROM skills WHERE id = %s"
+    values = [id]
+    result = run_sql(sql, values)[0]
+    if result is not None:
+        champion = champion_repository.select(result['champion_id'])
+        skill = Skill(champion, result['skill_name'], result['skill_shortcut'], result['id'] )
+    return skill
+    
+def select_all():
+    skills = []
+    sql = "SELECT * FROM skills"
+    results = run_sql(sql)
+    for row in results:
+        champion = champion_repository.select(row['champion_id'])
+        skill = Skill(champion, row['skill_name'], row['skill_shortcut'], row['id'])
+        skills.append(skill)
+    return skills
+
+def delete(id):
+    sql = "DELETE  FROM skills WHERE id = %s"
+    values = [id]
+    run_sql(sql, values)
+    
+def delete_all():
+    sql = "DELETE  FROM skills"
+    run_sql(sql)
+
+def update(skill):
+    sql = "UPDATE skills SET (champion_id, skill_name, skill_shortcut) = (%s, %s, %s) WHERE id = %s"
+    values = [skill.skill_name, skill.last_name, skill.id]
+    run_sql(sql, values)

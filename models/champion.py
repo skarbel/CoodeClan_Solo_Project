1addb94d0c456a7da5fc1862ac1299122ea85c5f
Andreas Skarpelos <95192235+Skarbel@users.noreply.github.com>
2022-01-28 11:34:41 +0000

    change of heart. New project for League of Legends

---
+++ models/champion.py
@@ -0,0 +1,7 @@
+class Champion:
+
+    def __init__(self, champion_name, champion_class, release_date, id = None):
+        self.champion_name = champion_name
+        self.champion_class = champion_class
+        self.release_date = release_date
+        self.id = id
--- models/customer.py
+++
@@ -1,7 +0,0 @@
-class Customer:
-
-    def __init__(self, customer_name, contact_details, registered, id = None):
-        self.customer_name = customer_name
-        self.contact_details = contact_details
-        self.registered = registered
-        self.id = id
